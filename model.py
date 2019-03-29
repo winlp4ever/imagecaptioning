399eef6c20f962ad3d3597a029202f1f77c7669a
ha quang le <winlp4ever@gmail.com>
2019-03-29 18:21:08 +0100

    resolve conflict

--- model.py
+++ model.py
@@ -118,16 +118,10 @@ class Captor(object):
 
         if os.path.isfile(path):
             print("=> loading checkpoint '{}'".format(path))
-<<<<<<< HEAD
-            if not torch.cuda.is_available():
-                checkpoint = torch.load(path, map_location='cpu')
-            checkpoint = torch.load(path)
-=======
             if torch.cuda.is_available():
                 checkpoint = torch.load(path)
             else:
                 checkpoint = torch.load(path, map_location='cpu')
->>>>>>> 78e76b5e77756fd167f440c6ff9e0594c7879244
             self.net.load_state_dict(checkpoint['state_dict'])
             self.optim.load_state_dict(checkpoint['optimizer'])
             print("=> loaded checkpoint '{}' (epoch {})"
